0b6a3a38c16ef32b25f0ebf12b8c3b447da97764
Brian Hines <brian@projectweekend.net>
2014-05-11 14:02:07 -0500

    Add example api route request handler

---
+++ api/user/index.js
@@ -0,0 +1,27 @@
+var async = require( 'async' );
+var ValidationManager = require( './validation' );
+var DataManager = require( './data' );
+var OutputManager = require( './output' );
+var utils = require( '../utils' );
+
+
+exports.create = function ( req, res ) {
+
+    async.waterfall( [
+        function ( callback ) {
+            ValidationManager.forCreate( req.body, callback );
+        },
+        function ( validatedReqBody, callback ) {
+            DataManager.create( validatedReqBody, callback );
+        },
+        function ( newUser, callback ) {
+            OutputManager.forCreate( newUser, callback );
+        }
+    ], function ( err, outputData ) {
+        if ( err ) {
+            return utils.handleRouteError( err, res );
+        }
+        return res.json( outputData, 201 );
+    } );
+
+};
