d71847edcdbc94ad80a0e466ae51804f8c3791e7
Brian Hines <brian@projectweekend.net>
2014-05-11 13:38:58 -0500

    Add express jet and mongoose

--- app.js
+++ app.js
@@ -2,18 +2,32 @@ var express = require( 'express' );
 var path = require( 'path' );
 var logger = require( 'morgan' );
 var bodyParser = require( 'body-parser' );
+var mongoose = require( 'mongoose' );
+var expressJwt = require( 'express-jwt' );
+
 
 var routes = require( './routes/index' );
 
+
 var app = express();
+
 
 app.use( logger( 'dev' ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded() );
 app.use( express.static( path.join( __dirname, 'public' ) ) );
+app.use( expressJwt( {
+    secret: process.env.JWT_SECRET,
+    skip: [
+        '/api/authenticate',
+        '/api/admin/authenticate'
+    ]
+} ) );
+
 
 // Use routes.index to map URLs to handlers in ./api
 app.use( '/', routes );
+
 
 /// catch 404 and forwarding to error handler
 app.use( function ( req, res, next ) {
@@ -22,7 +36,9 @@ app.use( function ( req, res, next ) {
     next( err );
 } );
 
+
 /// error handlers
+
 
 // development error handler
 // will print stacktrace
@@ -36,6 +52,7 @@ if ( app.get( 'env' ) === 'development' ) {
     } );
 }
 
+
 // production error handler
 // no stacktraces leaked to user
 app.use( function ( err, req, res, next ) {
@@ -45,6 +62,18 @@ app.use( function ( err, req, res, next ) {
         error: {}
     } );
 } );
+
+
+// connect to the db
+var MONGO_DB;
+var FIG_DB = process.env.DB_1_PORT;
+if ( FIG_DB ) {
+  MONGO_DB = FIG_DB.replace( "tcp", "mongodb" ) + "/dev_db";
+} else {
+  MONGO_DB = process.env.MONGO_URL;
+}
+mongoose.connect(MONGO_DB);
+var db = mongoose.connection;
 
 
 module.exports = app;
