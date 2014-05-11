9f11397d7ad722d13d529faba53d69b35f5ff083
Brian Hines <brian@projectweekend.net>
2014-05-11 14:04:38 -0500

    Map URL to handler for example

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+var UserAPI = require( '../api/user' );
+
+/* Map URLs to handlers in this file */
+router.post( '/api/signup', UserAPI.create );
 
 module.exports = router;
