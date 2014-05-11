20afe46168802b8799c463658893a09bbe1f97f8
Brian Hines <brian@projectweekend.net>
2014-05-11 13:29:00 -0500

    Formatting

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+var express = require( 'express' );
+var path = require( 'path' );
+var logger = require( 'morgan' );
+var bodyParser = require( 'body-parser' );
 
-var routes = require('./routes/index');
+var routes = require( './routes/index' );
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use( logger( 'dev' ) );
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded() );
+app.use( express.static( path.join( __dirname, 'public' ) ) );
 
 // Use routes.index to map URLs to handlers in ./api
-app.use('/', routes);
+app.use( '/', routes );
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+app.use( function ( req, res, next ) {
+    var err = new Error( 'Not Found' );
     err.status = 404;
-    next(err);
-});
+    next( err );
+} );
 
 /// error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
+if ( app.get( 'env' ) === 'development' ) {
+    app.use( function ( err, req, res, next ) {
+        res.status( err.status || 500 );
+        res.render( 'error', {
             message: err.message,
             error: err
-        });
-    });
+        } );
+    } );
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
+app.use( function ( err, req, res, next ) {
+    res.status( err.status || 500 );
+    res.render( 'error', {
         message: err.message,
         error: {}
-    });
-});
+    } );
+} );
 
 
 module.exports = app;
