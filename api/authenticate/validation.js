19178c4d8615c4a3558003ac3c957dfbd601124a
Brian Hines <brian@projectweekend.net>
2014-05-17 08:32:11 -0500

    Add request validation to authenticate api

---
+++ api/authenticate/validation.js
@@ -0,0 +1,17 @@
+var validator = require( 'validator' );
+var validationError = require( '../utils' ).validationError;
+
+
+exports.forAuthenticate = function ( requestBody, callback ) {
+
+    if ( !requestBody.email || !requestBody.password ) {
+        return callback( validationError( "Email and password are required" ) );
+    }
+
+    if ( requestBody.email && !validator.isEmail( requestBody.email ) ) {
+        return callback( validationError( "Email is not valid" ) );
+    }
+
+    return callback( null, requestBody );
+
+};
