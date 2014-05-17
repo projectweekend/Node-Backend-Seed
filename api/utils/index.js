74443b9c5cce8e598a534c4026a95d7d6c1c8f2e
Brian Hines <brian@projectweekend.net>
2014-05-17 08:43:13 -0500

    Add authorization error utility

--- api/utils/index.js
+++ api/utils/index.js
@@ -16,11 +16,23 @@ exports.systemError = function ( err ) {
 };
 
 
+exports.authorizationError = function ( message ) {
+    return {
+        err: null,
+        type: 'authorization',
+        message: message
+    };
+};
+
+
 exports.handleRouteError = function ( err, res ) {
     if ( err.type === 'validation' ) {
         return res.json( err, 400 );
     }
     if ( err.type === 'system' ) {
         return res.json( err, 500 );
+    }
+    if ( err.type === 'authorization' ) {
+        return res.json( err, 401 );
     }
 };
