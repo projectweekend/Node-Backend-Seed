461da4023a00dd3980a19e701f69b9350f40d46d
Brian Hines <brian@projectweekend.net>
2014-06-09 19:27:36 -0500

    Error handlers should return JSON

--- app.js
+++ app.js
@@ -42,11 +42,11 @@ app.use( function ( req, res, next ) {
 // will print stacktrace
 if ( app.get( 'env' ) === 'development' ) {
     app.use( function ( err, req, res, next ) {
-        res.status( err.status || 500 );
-        res.render( 'error', {
+        var status = err.status || 500;
+        res.json( {
             message: err.message,
             error: err
-        } );
+        }, status );
     } );
 }
 
@@ -54,8 +54,10 @@ if ( app.get( 'env' ) === 'development' ) {
 // production error handler
 // no stacktraces leaked to user
 app.use( function ( err, req, res, next ) {
-    res.status( err.status || 500 );
-    res.json( { message: err.message }, res.status );
+    var status = err.status || 500;
+    res.json( {
+        message: err.message
+    }, status );
 } );
 
 
