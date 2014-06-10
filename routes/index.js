2bf307f5a3dac15549df7e792306d5ee2de51a41
Brian Hines <brian@projectweekend.net>
2014-06-10 05:49:06 -0500

    Add function to restrict route access by role

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,19 @@ var SignupAPI = require( '../api/signup' );
 var AuthenticateAPI = require( '../api/authenticate' );
 
 
+var restrictByUserRole = function ( allowedLevels ) {
+    return function ( req, res, next ) {
+        if ( allowedLevels.indexOf( req.user.role ) > -1 ) {
+            next();
+        } else {
+            return res.json( {
+                message: "You are not authorized"
+            }, 403 );
+        }
+    };
+};
+
+
 /* Map URLs to handlers in this file */
 router.post( '/api/signup', SignupAPI.userSignup );
 router.post( '/api/authenticate', AuthenticateAPI.userLogin );
