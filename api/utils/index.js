92c95b20041aef9b1335f75ecff4699d2b300db9
Brian Hines <brian@projectweekend.net>
2014-06-18 11:05:31 -0500

    Remove old error messages

--- api/utils/index.js
+++ api/utils/index.js
@@ -1,30 +1,3 @@
-exports.validationError = function ( message ) {
-    return {
-        err: null,
-        type: 'validation',
-        message: message
-    };
-};
-
-
-exports.systemError = function ( err ) {
-    return {
-        err: err,
-        type: 'system',
-        message: "A system error occurred"
-    };
-};
-
-
-exports.authorizationError = function ( message ) {
-    return {
-        err: null,
-        type: 'authorization',
-        message: message
-    };
-};
-
-
 exports.handleRouteError = function ( err, res ) {
     if ( err.type === 'validation' ) {
         return res.json( err, 400 );
