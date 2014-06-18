833684afcae55efe041f44ade189dd1f23a3f88e
Brian Hines <brian@projectweekend.net>
2014-06-18 13:39:51 -0500

    Use express-validator in Signup module

--- api/signup/index.js
+++ api/signup/index.js
@@ -3,19 +3,33 @@ var Validation = require( './validation' );
 var Data = require( './data' );
 var Output = require( './output' );
 var utils = require( '../utils' );
+var validationError = require( '../utils/error_messages' ).validationError;
 
 
 exports.userSignup = function ( req, res ) {
 
     async.waterfall( [
         function ( callback ) {
-            Validation.forSignup( req.body, callback );
+
+            req.checkBody( 'email', "Must be an email address" ).isEmail();
+            req.checkBody( 'password', "Field is required" ).notEmpty();
+
+            var errors = req.validationErrors();
+            if ( errors ) {
+                return callback( errors );
+            }
+            return callback( null, req.body );
+
         },
-        function ( validatedReqBody, callback ) {
+        function ( requestBody, callback ) {
+
             Data.createUser( req.body.email, req.body.password, callback );
+
         },
         function ( newUserData, callback ) {
+
             Output.forSignup( newUserData, callback );
+
         }
     ], function ( err, outputData ) {
         if ( err ) {
--- api/utils/index.js
+++ api/utils/index.js
@@ -1,11 +1,17 @@
 exports.handleRouteError = function ( err, res ) {
+
     if ( err.type === 'validation' ) {
         return res.json( err, 400 );
     }
+
     if ( err.type === 'system' ) {
         return res.json( err, 500 );
     }
+
     if ( err.type === 'authorization' ) {
         return res.json( err, 401 );
     }
+
+    return res.json( err, 400 );
+
 };
