041f6f443027a1bdb2204d66977db172f7ca4641
Brian Hines <brian@projectweekend.net>
2014-07-25 22:21:32 -0500

    Simplify route error handler

--- api/utils/index.js
+++ api/utils/index.js
@@ -1,19 +1,5 @@
 exports.handleRouteError = function ( err, res ) {
 
-    var code = 400;
-    if ( err.type === 'authorization' ) {
-        code = 401;
-    }
-
-    if ( err.type === 'conflict' ) {
-        code = 409;
-    }
-
-    if ( err.type === 'system' ) {
-        code = 500;
-    }
-    delete err.type;
-
-    return res.json( err, code );
+    return res.json( err, err.code );
 
 };
